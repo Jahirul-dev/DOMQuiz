1cd1998965ee14d679e0f1ae3e06657bcb2d5cd1
Jahirul-dev <jahirul.jitu7@gmail.com>
2025-02-14 23:55:27 +0600

    add

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ let form = document.querySelector('.quiz-form')
 const answers = ["A","B","c"]
 
 form.addEventListener('submit', e=>{
-    e.preventDefault()
-})
+    e.preventDefault();
+    let score = 0;
+
+    let submitAnswers = [form.q1.value, form.q2.value]
+})   
