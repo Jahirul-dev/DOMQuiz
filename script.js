a6867f443774580125f5c7e1fd36fecfb7f61249
Jahirul-dev <jahirul.jitu7@gmail.com>
2025-02-15 07:35:06 +0600

    check

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-let form = document.querySelector('.quiz-form')
+var form = document.querySelector('.sub')
 const answers = ["A","B","c"]
 
-form.addEventListener('submit', e=>{
+form.addEventListener('click', e=>{
     e.preventDefault();
-    let score = 0;
-
-    let submitAnswers = [form.q1.value, form.q2.value]
-    
+    console.log('dastes');    
 })   
